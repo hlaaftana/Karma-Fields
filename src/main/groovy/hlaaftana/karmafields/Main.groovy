3b56e9098795fca0db9f94693c63d3897ae9d2bc
hlaaftana <atadorukaydin@gmail.com>
2016-06-25 12:52:47 +0300

    removed useless stuff

--- src/main/groovy/hlaaftana/karmafields/Main.groovy
+++ src/main/groovy/hlaaftana/karmafields/Main.groovy
@@ -108,86 +108,6 @@ formatUrl = { String url -> "\"$url\"" }
 formatFull = { DiscordObject obj -> "\"${obj.name.replace("\"", "\\\"")}\" ($obj.id)" }
 formatLongUser = { User user -> "\"$user.name\"#$user.discrim ($user.id)" }
 
-serverInfo = { Server server ->
-	[
-		Name: formatName(server),
-		ID: server.id,
-		Icon: formatUrl(server.icon),
-		"Creation date": server.createTime.format(dateFormat),
-		Members: [server.memberCount, server.members.size()].max(),
-		Roles: server.roles.size(),
-		Channels: "${server.channels.size()} total: ${server.textChannels.size()} text, ${server.voiceChannels.size()} voice",
-		"Latest member": formatFull(server.latestMember),
-		Region: formatName(server.region),
-		Owner: formatFull(server.owner),
-		"Default channel": formatFull(server.defaultChannel)
-	]
-}
-
-userInfo = { User user ->
-	[
-		Name: formatName(user) + "#$user.discrim",
-		ID: user.id,
-		Avatar: formatUrl(user.avatar),
-		"Creation date": user.createTime.format(dateFormat),
-		Bot: "$user.bot".capitalize(),
-		"Shared servers": user.sharedServers.size(),
-		Game: user.game ? (user.game.type == 1 ? "Streaming " : "Playing ") + user.game.name : "nothing",
-		Status: user.status.capitalize()
-	]
-}
-
-memberInfo = { Member member ->
-	userInfo(member) << [
-		"Join date": member.joinDate.format(dateFormat),
-		Mute: member.mute,
-		Deaf: member.deaf,
-		Roles: member.roles.collect { it.name.surround('"') }.with {
-			size() > 3 ? delegate[0..1] + (delegate[2] + "...") : delegate
-		}.join(", ") + " (${member.roles.size()})",
-		Color: "#" + Integer.toHexString(member.colorValue),
-		Nickname: member.rawNick
-	]
-}
-
-encode64Bits = { byte[] bytes ->
-	List byteLists = (bytes as List).collate(8)
-	List longList = byteLists.collect { List singularByteList ->
-		long collected = 0
-		singularByteList.each { byte byte_ ->
-			collected <<= 8
-			collected |= byte_
-		}
-		return collected
-	}
-	return longList as long[]
-}
-
-decode64Bits = { long[] longs ->
-	new String((longs as List).collect { long long_ ->
-		ByteBuffer.allocate(8).putLong(long_).array()
-	}.flatten() as byte[], "UTF-8")
-}
-
-compressString = { String decompressedString ->
-	List byteLists = (decompressedString.bytes as List).collate(2)
-	List charList = byteLists.collect { List singularByteList ->
-		short collected = 0
-		singularByteList.each { byte byte_ ->
-			collected <<= 8
-			collected |= byte_
-		}
-		collected as char
-	}
-	new String(charList as char[])
-}
-
-decompressString = { String compressedString ->
-	new String((compressedString.toCharArray() as List).collect { char char_ ->
-		ByteBuffer.allocate(2).putChar(char_).array()
-	}.flatten() as byte[], "UTF-8")
-}
-
 serverJson = { server ->
 	File file = new File("guilds/${DiscordObject.id(server)}.json")
 	if (!file.exists()) file.write("{}")
@@ -237,49 +157,10 @@ client.login("|><|New"){
 }
 bot = new CommandBot(triggers: [/\|>/, /></], type: BotType.REGEX, client: client)
 bot.loggedIn = true
-bot.client.eventThreadCount = 4
+client.eventThreadCount = 4
 
-class ArgumentChecker {
-	def arguments
-	ArgumentChecker(arguments){ this.arguments = arguments }
-	ArgumentChecker(String arguments){ this.arguments = arguments.tokenize() }
-
-	def getFirst(){ arguments[0] }
-	String getRest(){ arguments.drop(1).join(" ") }
-
-	static from(arguments, Closure closure){
-		closure.delegate = new ArgumentChecker(arguments)
-		closure.resolveStrategy = Closure.DELEGATE_FIRST
-		closure()
-	}
-
-	def first(what, Closure closure){
-		if (arguments[0].isCase(what)){
-			closure.delegate = new ArgumentChecker(arguments.drop(1))
-			closure.resolveStrategy = Closure.DELEGATE_FIRST
-			closure()
-		}
-	}
-
-	def starts(args, Closure closure){
-		if (arguments[0..args.size() - 1] == args){
-			closure.delegate = this
-			closure.resolveStrategy = Closure.DELEGATE_FIRST
-			closure()
-		}
-	}
-
-	def starts(String string, Closure closure){
-		if (arguments[0].isCase(string)){
-			closure.delegate = new ArgumentChecker(arguments)
-			closure.resolveStrategy = Closure.DELEGATE_FIRST
-			closure()
-		}
-	}
-}
-
-bot.client.listener("ready"){
-	bot.client.play("welcome to the afterworld.")
+client.listener(Events.READY){
+	client.play("welcome to the afterworld.")
 	if (!botReady){
 		Thread.start { LPClient.login(creds["lp_username"], creds["lp_pass"]) }
 		if (!me.token){
@@ -289,10 +170,10 @@ bot.client.listener("ready"){
 	}
 }
 
-bot.client.addListener "server create", {
-	if(it.server.id in bot.client.servers*.id) return
-	it.server.roles.find { it.name == "|><|" }?.edit(color: 0x0066c2)
-	it.server.defaultChannel.sendMessage("> Looks like I joined. Yay. Do `|>help` for commands.".block("verilog"))
+client.listener(Events.SERVER){
+	server.role("|><|")?.edit(color: 0x0066c2)
+	server.sendMessage(
+		"> Looks like I joined. Yay. Do `|>help` for commands.".block("verilog"))
 	File a = new File("guilds/${it.server.id}.json")
 	a.createNewFile()
 	if (a.text == ""){
@@ -300,7 +181,7 @@ bot.client.addListener "server create", {
 	}
 }
 
-bot.client.listener(Events.MESSAGE){
+client.listener(Events.MESSAGE){
 	markovFileThreadPool.submit {
 		File file = new File("markovs/${author.id}.txt")
 		if (!file.exists()) file.createNewFile()
@@ -309,7 +190,7 @@ bot.client.listener(Events.MESSAGE){
 }
 
 
-bot.client.listener(Events.MEMBER){
+client.listener(Events.MEMBER){
 	if (server.id in ["145904657833787392", "195223058544328704"]){
 		String message = """\
 > A member just joined:
@@ -317,7 +198,6 @@ bot.client.listener(Events.MEMBER){
 > ID: $member.id
 > Account creation time: $member.createTime
 > To member, type |>member, to ban, type |>ban, to bot, type |>bot.""".block("verilog")
-
 		server.defaultChannel.sendMessage(message)
 		server.textChannel("bot-mod-log")?.sendMessage(message)
 	}
@@ -381,7 +261,6 @@ bot.command("softban",
 			}else{
 				members = message.mentions.collect { message.server.member(it) }
 			}
-			//client.fields["popcorn_bot_bans"] += members
 			String staffOutput = "> The following members were softbanned by $author ($author.id):"
 			members.each {
 				it.ban(7)
@@ -389,7 +268,8 @@ bot.command("softban",
 				staffOutput += "\n> $it ($it.id)"
 				sendMessage("> Softbanned $it.".block("verilog"))
 			}
-			message.server.textChannel("171815384360419328").sendMessage(staffOutput.block("verilog"))
+			message.server.textChannel("bot-mod-log")
+				?.sendMessage(staffOutput.block("verilog"))
 		}catch (NoPermissionException ex){
 			sendMessage("> Failed to softban member(s). I don't seem to have permissions.".block("verilog"))
 		}
@@ -620,34 +500,6 @@ bot.command("automember",
 	}
 }
 
-bot.command("splitnum"){
-	ArgumentChecker.from(args.split(" ")){
-		def num = {
-			try{
-				Numbers.parseInteger(first)
-			}catch (NumberFormatException ex){
-				-1
-			}
-		}()
-		if (num < 0){
-			sendMessage("> Invalid number. Usage: |splitnum (number) (text)|".block("verilog"))
-			return
-		}
-		def output = (rest =~ /(?:.|\n){${num}}/).collect().inspect()
-		try{
-			sendMessage("> $output".block("verilog"))
-		}catch (ex){
-			Message eh = sendMessage("> Output exceeded 2000 characters. Uploading file...")
-			File file = new File("temp/splitnum_${System.currentTimeMillis()}.txt")
-			file.createNewFile()
-			file.write(output, "UTF-8")
-			sendFile(file)
-			eh.delete()
-			file.delete()
-		}
-	}
-}
-
 bot.command("member",
 	allowsPermissions: true){
 	try{
@@ -706,7 +558,6 @@ bot.command("ban",
 			}else{
 				members = message.mentions.collect { message.server.member(it) }
 			}
-			//client.fields["popcorn_bot_bans"] += members
 			String staffOutput = "> The following members were banned by $author ($author.id):"
 			members.each {
 				it.ban()
@@ -733,10 +584,13 @@ bot.command("eval"){
 	}else{
 		def evaluation = {
 			try{
-				return JSONUtil.parse(Unirest.post("http://groovyconsole.appspot.com/executor.groovy").field("script", args).asString().body)
+				JSONUtil.parse(
+					Unirest.post("http://groovyconsole.appspot.com/executor.groovy")
+					.field("script", args)
+					.asString().body)
 			}catch (ex){
 				sendMessage("> Failed to request evaluation.".block("verilog"))
-				return null
+				null
 			}
 		}()
 		if (evaluation == null) return
@@ -760,12 +614,6 @@ bot.command("eval"){
 	}
 }
 
-bot.command("julia"){
-	sendMessage(
-		("> " + Unirest.post("http://localhost:4040/eval").body(new String(args.getBytes("UTF-8"), "UTF-8")).asString().body)
-			.block("verilog"))
-}.whitelist("98457401363025920")
-
 ScriptingContainer cnt = new ScriptingContainer()
 bot.command("ruby"){
 	(([bot: bot, client: client, me: me, script: this] <<
@@ -778,37 +626,6 @@ bot.command("ruby"){
 	}
 	cnt.clear()
 }.whitelist("98457401363025920")
-
-bot.command("pura") {
-	try{
-		sendMessage(("> " + Pura.run(args, pura, false).toString()).block("verilog"))
-	}catch (ex){
-		sendMessage((ex.toString()).block("groovy"))
-	}
-}.whitelist("98457401363025920")
-
-bot.command("square") {
-	def text = args.toUpperCase()
-	def top = text.replace("", " ").substring(1)
-	def allText = "$top\n"
-	for (int i = 0; i < (text.substring(1).length() - 1); i++){
-		def string = text.substring(1)
-		def reversed = text.reverse().substring(1)
-		allText += string[i] + (" " * ((string.length() * 2) - 1)) + reversed[i] + "\n"
-	}
-	try{
-		sendMessage((allText + "${top.reverse().substring(1)}").block("verilog"))
-	}catch (ex){
-		sendMessage(("> Output surpassed 2000 characters.").block("verilog"))
-	}
-}
-
-bot.command("apichanges"){
-	client.server("81384788765712384") // discord api
-	.textChannel("124294271900712960") // api-changes
-	.logs.each { author.privateChannel.sendMessage(it.content); Thread.sleep(1000) }
-	sendMessage("> Done.".block("verilog"))
-}
 
 bot.command("color",
 	allowsPermissions: true){
@@ -872,162 +689,9 @@ bot.command("color",
 		sendMessage("> I don't have sufficient permissions. Give me Manage Roles.".block("verilog"))
 	}
 }
-
-bot.command("user"){
-	ArgumentChecker.from(args.tokenize()){
-		first(["i", "info"]){
-			User ass = author
-			first(["n", "name"]){
-				ass = message.private ?
-					client.members.find {
-						it.name.tokenize().join(" ") == rest.join(" ")
-					}.user :
-					message.server.members.find {
-						it.name.tokenize().join(" ") == rest.join(" ")
-					}
-			}
-			first(/\d+/){
-				ass = message.private ? client.user(first) : message.server.member(first)
-			}
-			first(User.MENTION_REGEX){
-				ass = message.private ? message.mentions[0] : message.server.member(message.mentions[0])
-			}
-			Map output = [:]
-			if (ass instanceof Member){
-				output << memberInfo(ass)
-			}else{
-				output << userInfo(ass)
-			}
-			sendMessage(output.collect { k, v ->
-				v ? "> $k: $v\n" : ""
-			}.join("").block("verilog"))
-		}
-	}
-}
-
-bot.command("server"){
-	List fullArgs = args.tokenize()
-	ArgumentChecker.from(fullArgs){
-		first(["i", "info"]){
-			Map output = [:]
-			first(null){
-				output << serverInfo(message.server)
-			}
-			first(["o", "owner"]){
-				output << memberInfo(message.server.owner)
-			}
-			sendMessage(output.collect { k, v ->
-				v ? "> $k: $v\n" : ""
-			}.join("").block("verilog"))
-		}
-		first(["m", "most"]){
-			first(["m", "member", "members"]){
-				first(["r", "role", "roles"]){
-					first(["c", "count"]){
-						int amount = {
-							try{
-								return first.toInteger()
-							}catch (ex){
-								return 5
-							}
-						}()
-						String output = "> The $amount members in this server with the highest role count are:"
-						message.server.members.sort { -it.roles.size() }[0..amount - 1].each {
-							output += "\n> $it: ${it.roles.size()}"
-						}
-						sendMessage(output.block("verilog"))
-						return
-					}
-				}
-			}
-		}
-	}
-}
-
-bot.command("channel"){
-	List fullArgs = args.split(" ")
-	ArgumentChecker.from(fullArgs){
-		first("clear"){
-			if (!author.fullPermissionsFor(message.channel).map["deleteMessages"]){
-				sendMessage("> You do not have sufficient permissions.".block("verilog"))
-				return
-			}
-			if (!message.server.me.fullPermissionsFor(message.channel).map["deleteMessages"]){
-				sendMessage("> I do not have sufficient permissions.".block("verilog"))
-				return
-			}
-			boolean fast = false
-			int count = 50
-			starts(/\d+/){
-				count = arguments[0].toInteger()
-			}
-			if (count > 10000 && !(author == me.user)){
-				sendMessage("> That is way too many messages.".block("verilog"))
-				return
-			}
-			Message initial = sendMessage("> Getting messages...".block("verilog"))
-			List messages = message.channel.getLogs(count + 1) - initial
-			boolean done = false
-			try{
-				message.channel.clear(messages)
-			}catch (NoPermissionException ex){
-				initial.edit("> I do not have sufficient permissions.".block("verilog"))
-				return
-			}
-			initial.edit("> Done.".block("verilog"))
-		}
-	}
-}
-
-bot.command("help", /\!/){
-	sendMessage("\n" * 2000).deleteAfter(2000)
-}.whitelist("110373943822540800")
-
 bot.command(["join", "oauth", "invite"]){
 	sendMessage(("> " + formatUrl(client.fields.botApp.inviteUrl(
 		new Permissions(manageRoles: true)))).block("verilog"))
-}
-
-bot.command("encodebin"){
-	String input = args
-	if ((input ==~ /\s*/) && !message.attachments.empty){
-		sendMessage("> Waiting a bit to download...".block("verilog"))
-		input = message.attachments[0].inputStream.text
-	}
-	if (input ==~ /\s*/){
-		sendMessage(("> No text specified. Either upload a file or add it to the message.").block("verilog"))
-	}
-	String output = input.toCharArray().collect { Integer.toBinaryString(it as int) }*.padLeft(8, "0").join(" ")
-	try{
-		sendMessage(("> " + output).block("verilog"))
-	}catch (ex){
-		sendMessage(("> Output exceeded 2000 characters. Attemptng to send file...").block("verilog"))
-		File dag = "temp/binary_encode_${System.currentTimeMillis()}.txt" as File
-		dag.createNewFile()
-		dag.write(output, "UTF-8")
-		sendFile(dag, filename: "encoded_binary.txt")
-	}
-}
-
-bot.command("decodebin"){
-	String input = args.replaceAll(/[^01 ]/, "")
-	if ((input ==~ /\s*/) && !message.attachments.empty){
-		sendMessage("> Waiting a bit to download...".block("verilog"))
-		input = message.attachments[0].inputStream.text.replaceAll(/[^01 ]/, "")
-	}
-	if (input ==~ /\s*/){
-		sendMessage(("> No text specified. Either upload a file or add it to the message.").block("verilog"))
-	}
-	String output = input.split(" ").collect { (Integer.parseInt(it, 2)) as char }.join("")
-	try{
-		sendMessage(("> " + output).block("verilog"))
-	}catch (ex){
-		sendMessage(("> Output exceeded 2000 characters. Attemptng to send file...").block("verilog"))
-		File dag = "temp/binary_decode_${System.currentTimeMillis()}.txt" as File
-		dag.createNewFile()
-		dag.write(output, "UTF-8")
-		sendFile(dag, filename: "decoded_binary.txt")
-	}
 }
 
 Random colorRandom = new Random()
@@ -1065,46 +729,22 @@ bot.command(/to\\u/){
 		}.sum()).block("verilog"))
 }
 
-bot.command("test"){
-	long gay = System.currentTimeMillis()
-	def (year, month, day, hour, minute, second, millisecond) =
-		message.rawTimestamp.split(/\D+/)*.toInteger()
-	def aa = new GregorianCalendar(year, month, day, hour, minute, second)
-	aa.set(Calendar.MILLISECOND, millisecond)
-	long recv = gay - aa.timeInMillis
-	long current = System.currentTimeMillis()
-	String a = "> Message receive time: ~${recv}ms"
-	Message ass = sendMessage(a.block("verilog"))
-	long send = System.currentTimeMillis() - current
-	current = System.currentTimeMillis()
-	a += "\n> Message send time: ~${send}ms"
-	ass.edit(a.block("verilog"))
-	long edit = System.currentTimeMillis() - current
-	a += "\n> Message edit time: ~${edit}ms"
-	ass.edit(a.block("verilog"))
-}
-
 bot.command(["help", "commands"]){
 	it.author.privateChannel.sendMessage("""> My prefix is `|>`, meaning when you call a command, you have to start it with `|>`.
 > Use this link if you want to invite me to your server: ${formatUrl(client.fields.botApp.inviteUrl(new Permissions(268435456)))}
 > My commands are:
 > |help/commands|: DMs you this message.
 > |color (hex color, rgb tuple or SVG named color)|: Creates a new role with zero permissions and assigns it that color, gives it the hex code of the color as a name and then assigns the role to you.
-> |square (text)|: Produces a square of the text you provded.
-> |game (mention)|: Gets the full name of the game the mentioned user is playing.
 > |markov|: Runs |markov (yourid)|.
 > |markov (mention|id)|: Generates a sentence from a Markov chain of the user's logged messages.
 > |markov everyone|: Generates a sentence from a Markov chain of every logged message.
 > |markov random|: Generates a sentence from a Markov chain of a random user's logged messages.
-> |splitnum (number) (text)|: Splits the text into (number) strings.
 > |eval (code)|: Evaluates (code) as Groovy code. Available for everyone now.
-> |apichanges|: DMs you the entire logs in the #apichanges channel in the Discord API server in case you're banned. **Do not try otherwise.**
 > |join/oauth/invite|: Gives you the link to inivte this bot.
-> |decodebin (binary numbers separated by spaces)|: Translates the given binary to text.
-> |encodebin (text)|: Encodes (text) to binary.""".block("verilog"))
+> I'm going to revamp this command shortly""".block("verilog"))
 }
 
-bot.client.metaClass.getIncludedEvents = {
+client.metaClass.getIncludedEvents = {
 	delegate.listenerSystem.listeners.keySet() as List
 }
 bot.initialize()
